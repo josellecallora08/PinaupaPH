c40804ff4f49de0a0f78a27e1e8a8bef6419139b
Soj <josellecallora08@gmail.com>
2024-01-11 15:22:09 +0800

    added update for household and cctv

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -6,8 +6,8 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
 
-const createToken = ({_id,username, role}) => {
-    return jwt.sign({_id,username,role}, process.env.SECRET, {expiresIn: "3d"})
+const createToken = ({user_id,username, role}) => {
+    return jwt.sign({user_id,username,role}, process.env.SECRET, {expiresIn: "3d"})
 }
 
 module.exports.sign_up = async (req, res) => {
@@ -60,85 +60,46 @@ module.exports.sign_in = async (req, res) => {
     try {
         const { username, password } = req.body;
         
-        let user_response = await USERMODEL.findOne({ username });
-        if (!user_response) {
-            user_response = await USERMODEL.findOne({ email: username });
-            if (!user_response) {
+        let response = await USERMODEL.findOne({ username });
+        if (!response) {
+            response = await USERMODEL.findOne({ email: username });
+            if (!response) {
                 return res.status(httpStatusCodes.BAD_REQUEST).json({ error: "Invalid Credentials (temp - Username)" });
             }
         }
 
-        const match = await bcrypt.compare(password, user_response.password);
+        const match = await bcrypt.compare(password, response.password);
         if (!match) {
             return res.status(httpStatusCodes.BAD_REQUEST).json({ error: "Invalid Credentials (temp - Password)" });
         }
 
-        const token = createToken(user_response._id, user_response.username, user_response.role);
+        const token = createToken(response._id, response.username, response.role);
 
-        return res.status(httpStatusCodes.OK).json({ msg: "Login Successfully!", role: user_response.role, token });
+        return res.status(httpStatusCodes.OK).json({ msg: "Login Successfully!", role: response.role, token });
     } catch (err) {
         console.error({ error: err.message });
         return res.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Server Error..." });
     }
 };
 
-module.exports.update_profile = async (req,res) => {
-    try{
-        const {_id} = req.params
-        const { name, username, email, password, mobile_no, birthday} = req.body;
-        let response = await USERMODEL.findById(_id)
-        if(!response){
-            return res.status(httpStatusCodes.BAD_REQUEST).json({error: "Failed to update information"})
-        }
-
-        const salt = await bcrypt.genSalt(10)
-        const hashed = await bcrypt.hash(password, salt)
-        if(!hashed){
-            return res.status(httpStatusCodes.BAD_REQUEST).json({error: "Failed to update the password"})
-        }
-        response = await USERMODEL.findByIdAndUpdate(_id, {name, username, email, password:hashed, mobile_no, birthday})
-        if(!response){
-            return res.status(httpStatusCodes.BAD_REQUEST).json({error: "Failed to update information(2)"})
-        }
-        return res.status(httpStatusCodes.CREATED).json({msg: "Information Updated Successfully!"})
-    }
-    catch(err){
-        console.error({error: err.message})
-        return res.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json({error: "Server Error..."});
-    }
-}
-
 module.exports.create_cctv = async (req, res) => {
     try {
-        const { _id } = req.params;
+        const { user_id } = req.params;
         const { name, username, password, port, ip_address } = req.body;
 
-        const userResponse = await USERMODEL.findById(_id);
+        const response = await OWNERMODEL.findOne({ user_id: user_id});
 
-        if (!userResponse) {
-            return res.status(httpStatusCodes.NOT_FOUND).json({ error: "User not found." });
+        if (!response) {
+            return res.status(httpStatusCodes.UNAUTHORIZED).json({ error: "Unable to CCTV Surveillance." });
         }
 
-        if (userResponse.role !== "Admin") {
-            return res.status(httpStatusCodes.UNAUTHORIZED).json({ error: "Only admin can add CCTV Camera." });
-        }
+        const details = { name, username, password, port, ip_address };
 
-        const userObjectId = userResponse._id.toString();
-
-        // Now you can use userObjectId in your queries
-        const ownerResponse = await OWNERMODEL.findOne({ user_id: userObjectId });
-
-        if (!ownerResponse) {
-            return res.status(httpStatusCodes.NOT_FOUND).json({ error: "Owner not found." });
-        }
-
-        const cctvDetails = { name, username, password, port, ip_address };
-
-        const index = ownerResponse.cctvs.findIndex(item => item.name.toString() === name);
+        const index = response.cctvs.findIndex(item => item.name.toString() === name);
 
         if (index === -1) {
-            ownerResponse.cctvs.push(cctvDetails);
-            await ownerResponse.save();
+            response.cctvs.push(details);
+            await response.save();
             return res.status(httpStatusCodes.CREATED).json({ msg: "Successfully added CCTV camera." });
         } else {
             return res.status(httpStatusCodes.BAD_REQUEST).json({ error: "CCTV already exists." });
@@ -180,13 +141,13 @@ module.exports.create_pet = async (req, res) => {
 
 module.exports.create_household = async (req, res) => {
     try{
-        const {_id} = req.params;
-        const {name, image, birthday, mobile_no} = req.body;
-        const response = await TENANTMODEL.findOne({user_id:_id})
+        const {user_id} = req.params;
+        const {name, relationship, birthday, mobile} = req.body;
+        const response = await TENANTMODEL.findOne({user_id:user_id})
         if(!response){
             return res.status(httpStatusCodes.BAD_REQUEST).json({error: "Invalid to create household..."}) 
         }
-        const details = {name, birthday, mobile_no}
+        const details = {name, relationship, birthday, mobile_no}
         const household_index = response.household.findIndex(item => item.name.toString() === name)
         if(household_index === -1){
             response.household.push(details)
@@ -220,5 +181,91 @@ module.exports.update_profile_picture = async (req, res) => {
     catch(err){
         console.error({error: err.message})
         return res.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json({error: `Server Error: ${err.message}`})
+    }
+}
+
+module.exports.update_profile = async (req,res) => {
+    try{
+        const {user_id} = req.params
+        const {name, username, email, password, mobile_no, birthday} = req.body;
+        let response = await USERMODEL.findById({user_id: user_id})
+        if(!response){
+            return res.status(httpStatusCodes.BAD_REQUEST).json({error: "Failed to update information"})
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hashed = await bcrypt.hash(password, salt)
+        if(!hashed){
+            return res.status(httpStatusCodes.BAD_REQUEST).json({error: "Failed to update the password"})
+        }
+        response = await USERMODEL.findByIdAndUpdate(_id, {name, username, email, password:hashed, mobile_no, birthday})
+        if(!response){
+            return res.status(httpStatusCodes.BAD_REQUEST).json({error: "Failed to update information(2)"})
+        }
+        return res.status(httpStatusCodes.CREATED).json({msg: "Information Updated Successfully!"})
+    }
+    catch(err){
+        console.error({error: err.message})
+        return res.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json({error: "Server Error..."});
+    }
+}
+
+module.exports.update_cctv = async (req, res) => {
+    try{
+        const {user_id, cctv_id} = req.params
+        const {name, username, password, port, ip_address} = req.body
+        const details = {name, username, password, port, ip_address}
+        
+        let response = await OWNERMODEL.findOne({user_id:user_id})
+        if(!response){
+            return res.status(httpStatusCodes.UNAUTHORIZED).json({error: "Unable Update Surveillance Information."})
+        }
+        
+        const index = response.cctvs.findIndex(item => item._id.toString() === cctv_id)
+        if(index === -1){
+            return res.status(httpStatusCodes.BAD_REQUEST).json({error: "Unable to locate CCTV"})
+        }
+        
+        Object.keys(details).forEach(field => {
+            if(details[field] !== undefined){
+                response.cctvs[index][field] = details[field]
+            }
+        })
+
+        await response.save()
+        return res.status(httpStatusCodes.OK).json({msg: "Successfully Updated!"})
+    }
+    catch(err){
+        console.error({ error: err.message });
+        return res.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Server Error." });
+    }
+}
+
+module.exports.update_household = async(req, res) =>{
+    try{
+        const {user_id, household_id} = req.params
+        const {name, relationship, birthday, mobile} = req.body
+        const details = {name, relationship, birthday, mobile}
+        
+        const response = await TENANTMODEL.findOne({user_id:user_id})
+        if(!response){
+            return res.status(httpStatusCodes.BAD_REQUEST).json({error: "Unable to Update."})
+        }
+        
+        const index = response.household.findIndex((item) => item._id.toString() === household_id)
+        if(index === -1){
+            return res.status(httpStatusCodes.UNAUTHORIZED.json({error: "Unable to Update Household Information."}))
+        }
+        Object.keys(details).forEach((detail) => {
+            if(details[detail] !== undefined){
+                response.household[index][detail] = details[detail]
+            }
+        })
+        await response.save()
+        return res.status(httpStatusCodes.OK).json({msg: "Information Updated..."})
+    }
+    catch(err){
+        console.error({error: err.message})
+        return res.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json({error: "Server Error..."})
     }
 }
