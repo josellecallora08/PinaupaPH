fc89725aeba43d627cd1844a01f8c91e3b58b09c
Soj <josellecallora08@gmail.com>
2024-03-14 21:00:41 +0800

    added report and issue

--- client/src/Page/Admin/Concern.jsx
+++ client/src/Page/Admin/Concern.jsx
@@ -2,7 +2,137 @@ import React from 'react'
 
 const Concern = () => {
   return (
-    <div>Concern</div>
+    <div className='h-full'>
+      <h1 className='uppercase font-bold p-5'>Concern and Issues</h1>
+      <div className='w-11/12 h-4/5 md:flex m-auto md:shadow-md rounded-md overflow-hidden'>
+          <div className='w-full h-full md:p-5 flex flex-col gap-5'>
+              <div className='h-auto max-h-auto flex flex-col gap-2'>
+                <div className='relative h-full w-full flex items-center justify-between gap-5 '>
+                  <div className='flex items-center gap-5 w-full '>
+                    <figure className='w-full h-full max-w-16 max-h-16'>
+                      <img src="" className='w-full h-full p-5 rounded-full bg-white' alt="" />
+                    </figure>
+                    <div className='w-full'>
+                      <p className='font-bold'>Roland Q. Angeles</p>
+                      <p className='text-sm'>Unit 001</p>
+                    </div>
+                  </div>
+                  <div className='absolute right-0 text-xs md:text-base'>
+                    January 20, 2024
+                  </div>
+                </div>
+                <div className='font-bold'>SINK LEAKING - Maintenance Report</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui fugiat voluptatibus quo laudantium, delectus aperiam aut necessitatibus iusto enim beatae, velit nihil suscipit ad rerum nulla officia excepturi ipsa dolorum quam non omnis? Distinctio voluptatum at soluta nesciunt tenetur earum odio est. Quisquam accusantium, eius est totam possimus quo vel dignissimos impedit!</div>
+              </div>
+              <div className='relative w-full h-full '>
+                <figure className='h-full w-full md:bg-white shadow-sm rounded-md'>
+                  <img src="" className='w-full h-full' alt="" />
+                </figure>
+                <button className='absolute top-1/2 left-5 w-full h-full max-w-10 max-h-10 p-2 rounded-full bg-gray'>{`<`}</button>
+                <button className='absolute top-1/2 right-5 w-full h-full max-w-10 max-h-10 p-2 rounded-full bg-gray'>{`>`}</button>
+              </div>
+          </div>
+          <div className='flex flex-col w-full h-full bg-red-200 p-5 border-l-2 border-gray/50'>
+                <div className='w-11/12 m-auto flex gap-3 items-center justify-center text-center p-5 border-b-2 border-gray/60'>
+                  <figure className='w-full h-full max-w-5 max-h-5'>
+                    <img src="" alt="" className='w-full h-full' />
+                  </figure>
+                  <span>Comments</span>
+                </div>
+                <div className='w-11/12  m-auto h-auto max-h-content max-h-max bg-no-repeat overflow-auto'>
+                  <div className='flex flex-col justify-end gap-5 h-auto w-full'>
+
+                    <div className='h-full min-h-20 w-full flex gap-2 items-center overflow-hidden'>
+                      <figure className='w-12 h-12 max-w-12  max-h-12 border rounded-full'>
+                        <img src=""  className="w-full h-full" alt="" />
+                      </figure>
+                      <div className='w-4/5 rounded-md bg-gray/20 p-3'>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio ratione adipisci autem qui vitae optio veniam odit, accusamus dolore aperiam?
+                      </div>
+                    </div>
+
+                    <div className='h-full min-h-5 w-full flex flex-row-reverse gap-2 items-center overflow-hidden'>
+                      <figure className='w-12 h-12 max-w-12  max-h-12 border rounded-full'>
+                        <img src=""  className="w-full h-full" alt="" />
+                      </figure>
+                      <div className='w-4/5 rounded-md bg-gray/20 p-3'>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio ratione adipisci autem qui vitae optio veniam odit, accusamus dolore aperiam?
+                      </div>
+                    </div>
+
+                    <div className='h-full min-h-5 w-full flex gap-2 items-center overflow-hidden'>
+                      <figure className='w-12 h-12 max-w-12  max-h-12 border rounded-full'>
+                        <img src=""  className="w-full h-full" alt="" />
+                      </figure>
+                      <div className='w-4/5 rounded-md bg-gray/20 p-3'>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio ratione adipisci autem qui vitae optio veniam odit, accusamus dolore aperiam?
+                      </div>
+                    </div>
+
+                    <div className='h-full min-h-5 w-full flex flex-row-reverse gap-2 items-center overflow-hidden'>
+                      <figure className='w-12 h-12 max-w-12  max-h-12 border rounded-full'>
+                        <img src=""  className="w-full h-full" alt="" />
+                      </figure>
+                      <div className='w-4/5 rounded-md bg-gray/20 p-3'>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio ratione adipisci autem qui vitae optio veniam odit, accusamus dolore aperiam?
+                      </div>
+                    </div>
+
+                    <div className='h-full min-h-5 w-full flex gap-2 items-center overflow-hidden'>
+                      <figure className='w-12 h-12 max-w-12  max-h-12 border rounded-full'>
+                        <img src=""  className="w-full h-full" alt="" />
+                      </figure>
+                      <div className='w-4/5 rounded-md bg-gray/20 p-3'>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio ratione adipisci autem qui vitae optio veniam odit, accusamus dolore aperiam?
+                      </div>
+                    </div>
+                    
+                    <div className='h-full min-h-5 w-full flex flex-row-reverse gap-2 items-center overflow-hidden'>
+                      <figure className='w-12 h-12 max-w-12  max-h-12 border rounded-full'>
+                        <img src=""  className="w-full h-full" alt="" />
+                      </figure>
+                      <div className='w-4/5 rounded-md bg-gray/20 p-3'>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio ratione adipisci autem qui vitae optio veniam odit, accusamus dolore aperiam?
+                      </div>
+                    </div>
+
+                    <div className='h-full min-h-5 w-full flex gap-2 items-center overflow-hidden'>
+                      <figure className='w-12 h-12 max-w-12  max-h-12 border rounded-full'>
+                        <img src=""  className="w-full h-full" alt="" />
+                      </figure>
+                      <div className='w-4/5 rounded-md bg-gray/20 p-3'>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio ratione adipisci autem qui vitae optio veniam odit, accusamus dolore aperiam?
+                      </div>
+                    </div>
+                    
+                    <div className='h-full min-h-5 w-full flex flex-row-reverse gap-2 items-center overflow-hidden'>
+                      <figure className='w-12 h-12 max-w-12  max-h-12 border rounded-full'>
+                        <img src=""  className="w-full h-full" alt="" />
+                      </figure>
+                      <div className='w-4/5 rounded-md bg-gray/20 p-3'>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio ratione adipisci autem qui vitae optio veniam odit, accusamus dolore aperiam?
+                      </div>
+                    </div>
+
+                    <div className='h-full min-h-5 w-full flex gap-2 items-center overflow-hidden'>
+                      <figure className='w-12 h-12 max-w-12  max-h-12 border rounded-full'>
+                        <img src=""  className="w-full h-full" alt="" />
+                      </figure>
+                      <div className='w-4/5 rounded-md bg-gray/20 p-3'>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio ratione adipisci autem qui vitae optio veniam odit, accusamus dolore aperiam?
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+                <div className='w-11/12 h-auto m-auto min-h-20 max-h-32'>
+                  <form className='h-full w-full rounded-md overflow-hidden'>
+                      <textarea name="" id="" placeholder="Send Message" className='w-full h-full outline-none border-2 border-gray p-5'></textarea>
+                  </form>
+                </div>
+          </div>
+      </div>
+    </div>
   )
 }
 
