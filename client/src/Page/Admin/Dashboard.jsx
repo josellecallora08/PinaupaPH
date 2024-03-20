9898319426ae581b862063d74b2dbec2f610b7b5
Soj <josellecallora08@gmail.com>
2024-03-20 21:12:07 +0800

    change dashboard

--- client/src/Page/Admin/Dashboard.jsx
+++ client/src/Page/Admin/Dashboard.jsx
@@ -58,19 +58,22 @@ const Dashboard = () => {
           <h1 className='uppercase font-bold py-1'>Dashboard</h1>
         </div>
         <div className="w-11/12 h-full m-auto grid grid-cols-3 gap-2 grid-rows-7">
-          <div className='hidden md:flex col-span-3 row-span-1 bg-primary'>
+          <div className='hidden md:flex col-span-3 row-span-1  rounded-md overflow-hidden shadow-md bg-primary/20'>
+            <figure className='w-full h-full max-w-12 md:max-w-60'>
+              <img src={City} className='w-full h-full object-contain' alt="" />
+            </figure>
           </div>
-          <div className='order-last md:order-none col-span-3 row-span-4 grid grid-cols-3 grid-rows-1 gap-2'>
-            <div className='col-span-3 md:col-span-2 row-span-1 w-full h-full bg-white flex items-center justify-center flex-col'>
+          <div className='order-last md:order-none col-span-3 row-span-4 grid grid-cols-3 grid-rows-1 gap-2 '>
+            <div className='col-span-3 md:col-span-2 row-span-1 w-full h-full bg-white flex items-center justify-center flex-col rounded-md overflow-hidden shadow-md'>
               <div className='w-11/12 m-auto py-2'>
                 <p className='font-bold text-[#9e9e9e] lg:text-xl'>Revenue Overview</p>
               </div>
-              <figure className='flex justify-center items-center h-auto md:h-full w-full pb-5 max-h-40 lg:max-h-max max-w-[90%] m-auto'>
+              <figure className='flex justify-center items-center h-auto md:h-full w-full  max-h-max lg:max-h-max max-w-[90%] m-auto'>
                 {/* <LineChart data={{"2021-01-01": 11, "2021-01-02": 6}} width="100%" height="100%"/> */}
                 <Bar data={data} options={options}/>
               </figure>
             </div>
-            <div className='bg-white pb-3 col-span-3 md:col-span-1'>
+            <div className='bg-white pb-3 col-span-3 md:col-span-1 rounded-md overflow-hidden shadow-md'>
               <div className='w-11/12 h-full m-auto flex flex-col group'>
                 <h1 className='text-[#9e9e9e] font-semibold py-2 lg:text-xl'>Recent Activity</h1>
                 <div className=' w-full h-full overflow-hidden md:group-hover:overflow-y-scroll'>
@@ -222,7 +225,7 @@ const Dashboard = () => {
           </div>
           <div className='col-span-3 row-span-2 grid grid-cols-4 md:grid-cols-3 grid-flow-row md:grid-rows-2 gap-2'>
             <div className='col-span-4 md:col-span-2 row-span-2 grid grid-cols-2 grid-rows-2 gap-2'>
-              <div className='col-span-1 row-span-1 bg-white flex'>
+              <div className='col-span-1 row-span-1 bg-white flex rounded-md overflow-hidden shadow-md'>
                 <div className='w-full bg-primary max-w-20 flex items-center justify-center'>
                   <div className='rounded-full p-3 bg-gray'>
                     <figure className='w-full h-full max-w-16 max-h-16'>
@@ -231,15 +234,24 @@ const Dashboard = () => {
                   </div>
                 </div>
                 <div className='w-full h-full'>
-                  <div className='relative w-11/12 m-auto h-full'>
-                    <span className='absolute md:top-2 right-0 text-end text-[#9e9e9e] text-xs font-semibold  xl:text-sm 2xl:text-lg'>Total Amount</span>
+                  <div className='relative w-11/12 m-auto h-full flex items-center'>
+                    {/* <span className='absolute md:top-2 right-0 text-end text-[#9e9e9e] text-xs font-semibold  xl:text-sm 2xl:text-lg'>Total Amount</span>
                     <div className='w-full h-full flex items-center justify-center'>
                       <span className='text-lg md:text-4xl lg:text-5xl font-bold'>9,000</span>
+                    </div> */}
+                    <div className=' w-full xl:flex flex-col gap-5'>
+                      <div className='xl:flex justify-between'>
+                        <p className='text-[#9e9e9e] text-xs text-center xl:text-base font-semibold'>Total Paid</p>
+                        <p className='font-bold text-center text-xl xl:text-4xl'>9,000.00</p>
+                      </div>
+                      <p className={`relative w-full h-2 bg-primary/20 rounded-full overflow-hidden shadow-inner`}>
+                        <span className={`absolute w-1/4 h-2 bg-primary`}></span>
+                      </p>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className='col-span-1 row-span-1 bg-white flex'>
+              <div className='col-span-1 row-span-1 bg-white flex rounded-md overflow-hidden shadow-md'>
                 <div className='bg-primary w-full max-w-20 flex items-center justify-center'>
                 <div className='rounded-full p-3 bg-gray'>
                     <figure className='w-full h-full max-w-16 max-h-16'>
@@ -248,15 +260,24 @@ const Dashboard = () => {
                   </div>
                 </div>
                 <div className='w-full h-full'>
-                  <div className='relative w-11/12 m-auto h-full'>
-                    <span className='absolute md:top-2 right-0 text-end text-[#9e9e9e] text-xs font-semibold xl:text-sm 2xl:text-lg'>Occupancy Rate</span>
+                  <div className='relative w-11/12 m-auto h-full flex items-center'>
+                    {/* <span className='absolute md:top-2 right-0 text-end text-[#9e9e9e] text-xs font-semibold xl:text-sm 2xl:text-lg'>Occupancy Rate</span>
                     <div className='w-full h-full flex items-center justify-center'>
                       <span className='text-lg md:text-4xl lg:text-5xl font-bold'>96%</span>
+                    </div> */}
+                    <div className=' w-full xl:flex flex-col gap-5'>
+                      <div className='xl:flex justify-between'>
+                        <p className='text-[#9e9e9e] text-xs text-center xl:text-base font-semibold'>Deliquency Rate</p>
+                        <p className='font-bold text-center text-xl xl:text-4xl'>96%</p>
+                      </div>
+                      <p className={`relative w-full h-2 bg-primary/20 rounded-full overflow-hidden shadow-inner`}>
+                        <span className={`absolute w-[96%] h-2 bg-primary`}></span>
+                      </p>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className='col-span-1 row-span-1 bg-white flex'>
+              <div className='col-span-1 row-span-1 bg-white flex rounded-md overflow-hidden shadow-md'>
                 <div className='bg-primary w-full max-w-20 flex items-center justify-center'>
                   <div className='rounded-full p-3 bg-gray'>
                     <figure className='w-full h-full max-w-16 max-h-16'>
@@ -265,15 +286,24 @@ const Dashboard = () => {
                   </div>
                 </div>
                 <div className='w-full h-full'>
-                  <div className='relative w-11/12 m-auto h-full'>
-                    <span className='absolute md:top-2 right-0 text-end text-[#9e9e9e] text-xs font-semibold xl:text-sm 2xl:text-lg'>Deliquency Rate</span>
+                  <div className='relative w-11/12 m-auto h-full flex items-center'>
+                    {/* <span className='absolute md:top-2 right-0 text-end text-[#9e9e9e] text-xs font-semibold xl:text-sm 2xl:text-lg'>Deliquency Rate</span>
                     <div className='w-full h-full flex items-center justify-center'>
                       <span className='text-lg md:text-4xl lg:text-5xl font-bold'>96%</span>
+                    </div> */}
+                    <div className=' w-full xl:flex flex-col gap-5'>
+                      <div className='xl:flex justify-between'>
+                        <p className='text-[#9e9e9e] text-xs text-center xl:text-base font-semibold'>Occupancy Rate</p>
+                        <p className='font-bold text-center text-xl xl:text-4xl'>54%</p>
+                      </div>
+                      <p className={`relative w-full h-2 bg-primary/20 rounded-full overflow-hidden shadow-inner`}>
+                        <span className={`absolute w-[${'54'}%] h-2 bg-primary`}></span>
+                      </p>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className='col-span-1 row-span-1 bg-white flex'>
+              <div className='col-span-1 row-span-1 bg-white flex rounded-md overflow-hidden shadow-md'>
                 <div className='bg-primary w-full max-w-20 flex items-center justify-center'>
                   <div className='rounded-full p-3 bg-gray'>
                     <figure className='w-full h-full max-w-6 max-h-6'>
@@ -281,17 +311,26 @@ const Dashboard = () => {
                     </figure>
                   </div>
                 </div>
-                <div className='w-full h-full'>
-                  <div className='relative w-11/12 m-auto h-full'>
-                    <span className='absolute md:top-2 right-0 text-end text-[#9e9e9e] text-xs font-semibold xl:text-sm 2xl:text-lg'>Renewal Rate</span>
+                <div className='w-full h-full '>
+                  <div className='relative w-11/12 m-auto h-full flex items-center'>
+                    {/* <span className='absolute md:top-2 right-0 text-end text-[#9e9e9e] text-xs font-semibold xl:text-sm 2xl:text-lg'>Renewal Rate</span>
                     <div className='w-full h-full flex items-center justify-center'>
                       <span className='text-lg md:text-4xl lg:text-5xl font-bold'>96%</span>
+                    </div> */}
+                    <div className=' w-full xl:flex flex-col gap-5'>
+                      <div className='xl:flex justify-between'>
+                        <p className='text-[#9e9e9e] text-xs text-center xl:text-base font-semibold'>Renewal Rate</p>
+                        <p className='font-bold text-center text-xl xl:text-4xl'>66%</p>
+                      </div>
+                      <p className={`relative w-full h-2 bg-primary/20 rounded-full overflow-hidden shadow-inner`}>
+                        <span className={`absolute w-[${'66'}%] h-2 bg-primary`}></span>
+                      </p>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div className='col-span-4 md:col-span-1 row-span-2 pb-3 bg-white'>
+            <div className='col-span-4 md:col-span-1 row-span-2 pb-3 bg-white rounded-md overflow-hidden shadow-md'>
               <div className='w-11/12 h-full m-auto flex flex-col'>
                 <h1 className='text-[#9e9e9e] font-semibold py-2 lg:text-lg'>Concern and Issue</h1>
                 <div className='w-full h-full flex items-center bg-white'>
