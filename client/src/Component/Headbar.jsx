6d8706d00dd914fa2a62c6dff549bf2d9673f223
Soj <josellecallora08@gmail.com>
2024-03-21 21:26:41 +0800

    updated dashboard

--- client/src/Component/Headbar.jsx
+++ client/src/Component/Headbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import pfp from '/pfp.svg'
 import { useState } from 'react'
 import { TbBellRinging } from "react-icons/tb";
@@ -12,6 +12,8 @@ import { logout } from '../features/authentication';
 const Headbar = () => {
   const sidebar = useSelector(state => state.toggle.sidebar) //for sidebar ternary
   const profile = useSelector(state => state.toggle.profile) //for profile ternary
+  const [currentDate, setCurrentDate] = useState(new Date());
+
   const dispatch = useDispatch()
 
   const handleLogout = () => {
@@ -38,16 +40,40 @@ const Headbar = () => {
   
     }
   ]
-    
+  const month = ['January', 'February', 'March', 'Apri', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+  const day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentDate(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const date = new Date()
+  const Month = date.getMonth()
+  const Day = date.getDay()
+  const Today = date.getDate()
+  const Year = date.getFullYear()
+  const Hour = date.getHours()
+  const Minute = date.getMinutes()
+  const Second = date.getSeconds()
+  const completeDate = `${day[Day]}, ${month[Month]} ${Today < 10 ? `0${Today}` : Today}, ${Year} ${Hour >= 12 ? `${Hour < 10 ? `0${Hour}` : Hour}:${Minute}:${Second < 10 ? `0${Second}` : Second}PM`: `${Hour < 10 ? `0${Hour}` : Hour}:${Minute}:${Second < 10 ? `0${Second}` : Second}AM`} `
   return (
     
 <div className='w-full h-full max-h-20 sticky top-0 z-20 bg-primary'>
   <div className=' flex justify-between items-center p-5 w-full relative m-auto  '>
+    <div className='flex items-center gap-5'>
     <button onClick={handleSidebar} className='flex items-center'>
       <figure className='w-full h-full max-w-5 max-h-5'>
         <img src={sidebar ? close : menu} className='w-full h-full' alt="" />
       </figure>
     </button>
+    <div>
+      <span className='text-gray font-semibold'>{completeDate}</span>
+    </div>
+    </div>
       <div className='flex items-center'>
         <TbBellRinging size={25} color='white' />
         <img src={pfp} alt='' className=' w-10 h-10 rounded-full ml-2  cursor-pointer' onClick={handleProfile} />
--- client/src/Page/Admin/Dashboard.jsx
+++ client/src/Page/Admin/Dashboard.jsx
@@ -56,24 +56,27 @@ const Dashboard = () => {
   }
   return (
     <>
-      <div className="w-full h-full xl:h-full xl:max-h-screen flex flex-col bg-gray pb-2">
+      <div className="w-full h-full xl:h-full xl:max-h-auto flex flex-col bg-gray pb-2">
         <div className='w-11/12 h-fit m-auto py-2'>
           <h1 className='uppercase font-bold py-1'>Dashboard</h1>
         </div>
         <div className="w-11/12 xl:max-h-[800px] flex flex-col m-auto xl:grid grid-cols-3 gap-2 grid-rows-7">
-          <div className='min-h-20 md:flex col-span-3 row-span-1  rounded-md overflow-hidden shadow-md bg-primary/20'>
-            <figure className='w-full h-full max-w-max md:max-w-60'>
+          <div className='flex justify-between items-center col-span-3 row-span-1  rounded-md overflow-hidden shadow-md bg-primary/20'>
+            <figure className='w-full h-full max-w-max md:max-w-fit'>
               <img src={City} className='w-full h-full object-contain' alt="" />
             </figure>
+            <div className='pr-5'>
+              <span className='text-base xl:text-2xl font-semibold'>Hey, Joselle E. Callora</span>
+            </div>
           </div>
           <div className='order-last md:order-none col-span-3 row-span-4 flex flex-col xl:grid grid-cols-3 grid-rows-1 gap-2 '>
             <div className='col-span-3 md:col-span-2 row-span-auto xl:row-span-1 bg-white h-fit md:h-full flex items-center justify-center flex-col rounded-md overflow-hidden shadow-md'>
-              <div className='w-11/12 h-fit xl:h-full max-h-20 m-auto py-2'>
+              <div className='w-11/12 h-fit  xl:max-h-fit m-auto md:py-2'>
                 <p className='font-bold text-[#9e9e9e] lg:text-xl'>Revenue Overview</p>
               </div>
               <div className='w-full h-fit xl:h-full flex'>
                 <figure className='flex justify-center items-center h-full w-11/12 xl:w-11/12 m-auto'>
-                  <Bar data={data} options={options}/>
+                  <Bar data={data} options={options} className='py-2 md:py-5 h-60 md:h-auto'/>
                 </figure>
               </div>
             </div>
@@ -83,7 +86,7 @@ const Dashboard = () => {
                 <div className=' w-full h-full overflow-hidden md:group-hover:overflow-y-scroll'>
                   <div className='w-full h-auto md:max-h-[200px]'>
                    
-                    <div className='flex justify-between p-2 hover:bg-gray'>
+                    <div className='flex justify-between p-2 rounded-full md:rounded-md hover:bg-gray'>
                       <article className='flex items-center gap-2'>
                         <figure className='w-full h-full max-w-10 max-h-10 overflow-hidden'>
                           <img src={user} className='w-full h-full object-contain' alt="" />
@@ -125,7 +128,7 @@ const Dashboard = () => {
                         <p className='font-bold text-center text-xl xl:text-4xl'>9,000.00</p>
                       </div>
                       <p className={`relative w-11/12 m-auto xl:w-full h-2 bg-primary/20 rounded-full overflow-hidden shadow-inner`}>
-                        <span className={`absolute w-1/4 h-2 bg-primary`}></span>
+                        <span className={`absolute w-1/4 h-2 bg-primary animate-in slide-in-from-left-20 duration-1000`}></span>
                       </p>
                     </div>
                   </div>
@@ -151,7 +154,7 @@ const Dashboard = () => {
                         <p className='font-bold text-center text-xl xl:text-4xl'>96%</p>
                       </div>
                       <p className={`relative w-11/12 m-auto xl:w-full h-2 bg-primary/20 rounded-full overflow-hidden shadow-inner`}>
-                        <span className={`absolute w-[96%] h-2 bg-primary`}></span>
+                        <span className={`absolute w-[96%] h-2 bg-primary animate-in slide-in-from-left-20 duration-1000`}></span>
                       </p>
                     </div>
                   </div>
@@ -177,7 +180,7 @@ const Dashboard = () => {
                         <p className='font-bold text-center text-xl xl:text-4xl'>54%</p>
                       </div>
                       <p className={`relative w-11/12 m-auto xl:w-full h-2 bg-primary/20 rounded-full overflow-hidden shadow-inner`}>
-                        <span className={`absolute ${prc} h-2 bg-primary`}></span>
+                        <span className={`absolute ${prc} h-2 bg-primary animate-in slide-in-from-left-20 duration-1000`}></span>
                       </p>
                     </div>
                   </div>
@@ -203,7 +206,7 @@ const Dashboard = () => {
                         <p className='font-bold text-center text-xl xl:text-4xl'>66%</p>
                       </div>
                       <p className={`relative w-11/12 m-auto xl:w-full h-2 bg-primary/20 rounded-full overflow-hidden shadow-inner`}>
-                        <span className={`absolute w-[${'66'}%] h-2 bg-primary`}></span>
+                        <span className={`absolute w-[${'66'}%] h-2 bg-primary animate-in slide-in-from-left-20 duration-1000`}></span>
                       </p>
                     </div>
                   </div>
@@ -215,7 +218,7 @@ const Dashboard = () => {
                 <h1 className='text-[#9e9e9e] font-semibold py-2 lg:text-lg'>Concern and Issue</h1>
                 <div className='w-full h-full flex items-center bg-white'>
                   {/* <BarChart data={data} width="90%" height="80%"/> */}
-                  <CircularProgressbar className='mx-auto lg:w-40 md:mx-auto md:w-40 w-32 mt-4' value={percentage} minValue={0} maxValue={maxValue} text={percentage +'/'+ maxValue} strokeWidth={15} styles={buildStyles({textColor: '#183044',pathColor: '#183044', trailColor: '#E7E8E9' })} />
+                  <CircularProgressbar className='mx-auto lg:w-40 md:mx-auto md:w-40 w-32 mt-4 ' value={percentage} minValue={0} maxValue={maxValue} text={percentage +'/'+ maxValue} strokeWidth={15} styles={buildStyles({textColor: '#183044',pathColor: '#183044', trailColor: '#E7E8E9' })} />
                 </div>
               </div>
             </div>
