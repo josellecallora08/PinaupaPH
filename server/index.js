047eadd116e78437d8daf381223f28d1b2ebd0cb
Soj <josellecallora08@gmail.com>
2024-04-08 04:47:17 +0800

    added localhost for development

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json())
 
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL],
+    origin: [process.env.CLIENT_URL, "http://localhost:5173"],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     optionsSuccessStatus: 204,
