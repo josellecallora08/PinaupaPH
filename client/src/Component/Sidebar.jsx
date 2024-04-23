e7f3f043ee06b2378d7de63850f6b0a7d6a7bdbe
Soj <josellecallora08@gmail.com>
2024-04-23 19:56:52 +0800

    finished active status of navigations

--- client/src/Component/Sidebar.jsx
+++ client/src/Component/Sidebar.jsx
@@ -6,15 +6,23 @@ import { isLogout } from '../features/authentication'
 import logo from '/logo.svg'
 import m_logo from '/m_logo.svg'
 import dashboard from '/dashboard.svg'
+import dashboard_ from '/dashboard_white.svg'
 import tenant from '/tenant.svg'
-import apartment from '/apartments.svg'
+import tenant_ from '/tenant_white.svg'
+import apartment from '/apartment.svg'
+import apartment_ from '/apartment_white.svg'
 import security from '/Security.svg'
-import document_svg from '/document.svg'
+import documents from '/document.svg'
+import document_ from '/document_white.svg'
 import report from '/concern.svg'
+import report_ from '/concern_white.svg'
 import down from '/down.svg'
 import contact from '/contact.svg'
+import contact_ from '/contact_white.svg'
 import term from '/terms.svg'
+import term_ from '/terms_white.svg'
 import logout from '/logout.svg'
+// import logout_ from '/logout_white.svg'
 const Sidebar = () => {
   const sidebarRef = useRef(null);
   const menu = useSelector((state) => state.toggle.sidebar)
@@ -72,15 +80,15 @@ const Sidebar = () => {
             <li className="w-full h-fit ">
               <Link
                 to={'/dashboard'}
-                className={`flex items-center center w-4/5 m-auto h-full hover:bg-primary-color/5 rounded-md ${menu ? 'p-5' : 'p-3'} ${isActive('/dashboard') ? 'bg-primary-color' : ''} `}
+                className={`flex items-center ${!menu && 'justify-center' } w-4/5 gap-5 m-auto h-full hover:bg-primary-color/5 rounded-md ${menu ? 'p-5' : 'p-3'} ${isActive('/dashboard') ? 'bg-primary-color' : ''} `}
               >
                 <figure
-                  className={`${menu ? '' : 'flex justify-center'} max-w-10 w-full h-full`}
+                  className={`${menu ? '' : 'flex justify-center'} max-w-5 max-h-5 w-full h-full`}
                 >
                   <img
-                    src={dashboard}
+                    src={isActive('/dashboard') ? dashboard_ : dashboard}
                     alt=""
-                    className="max-w-5 max-h-5 object-contain"
+                    className="size-full object-contain"
                   />
                 </figure>
                 {menu ? (
@@ -98,19 +106,20 @@ const Sidebar = () => {
                 <li className="w-full h-fit ">
                   <Link
                     to={'/tenant'}
-                    className={`flex items-center center w-4/5  m-auto h-full hover:bg-primary-color/5 rounded-md ${menu ? 'p-5' : 'p-3'} `}
+                    className={`flex items-center ${!menu && 'justify-center' } w-4/5 gap-5 m-auto h-full hover:bg-primary-color/5 rounded-md ${menu ? 'p-5' : 'p-3'} 
+                    ${isActive('/tenant') ? 'bg-primary-color' : ''} `}
                   >
                     <figure
-                      className={`${menu ? '' : 'flex justify-center'} max-w-10 w-full h-full`}
+                      className={`${menu ? '' : 'flex justify-center'} max-w-5 max-h-5 w-full h-full`}
                     >
                       <img
-                        src={tenant}
+                        src={isActive('/tenant') ? tenant_ : tenant}
                         alt=""
-                        className="max-w-5 max-h-5 object-contain"
+                        className="size-full object-contain"
                       />
                     </figure>
                     {menu ? (
-                      <span className="font-semibold text-primary-color">
+                      <span className={`font-semibold text-primary-color ${isActive('/tenant') && 'text-white'}`}>
                         Tenant
                       </span>
                     ) : (
@@ -121,19 +130,19 @@ const Sidebar = () => {
                 <li className="w-full h-fit">
                   <Link
                     to={'/apartment'}
-                    className={`flex items-center center w-4/5  m-auto h-full hover:bg-primary-color/5 rounded-md ${menu ? 'p-5' : 'p-3'}`}
+                    className={`flex items-center ${!menu && 'justify-center' } w-4/5 gap-5 m-auto h-full hover:bg-primary-color/5 rounded-md ${menu ? 'p-5' : 'p-3'} ${isActive('/apartment') ? 'bg-primary-color' : ''}`}
                   >
                     <figure
-                      className={`${menu ? '' : 'flex justify-center'} max-w-10 w-full h-full`}
+                      className={`${menu ? '' : 'flex justify-center'} max-w-5 max-h-5 w-full h-full`}
                     >
                       <img
-                        src={apartment}
+                        src={isActive('/apartment') ? apartment_ : apartment}
                         alt=""
-                        className="max-w-5 max-h-5 object-contain"
+                        className="size-full object-contain"
                       />
                     </figure>
                     {menu ? (
-                      <span className="font-semibold text-primary-color">
+                      <span className={`font-semibold text-primary-color ${isActive('/apartment') && 'text-white'}`}>
                         Apartment
                       </span>
                     ) : (
@@ -148,19 +157,19 @@ const Sidebar = () => {
             <li className="w-full h-fit">
               <Link
                 to={`/security`}
-                className={`flex items-center center w-4/5  m-auto h-full hover:bg-primary-color/5 rounded-md ${menu ? 'p-5' : 'p-3'}`}
+                className={`flex items-center ${!menu && 'justify-center' } w-4/5  m-auto h-full hover:bg-primary-color/5 rounded-md ${menu ? 'p-5' : 'p-3'} ${isActive('/security') ? 'bg-primary-color' : ''}`}
               >
                 <figure
                   className={`${menu ? '' : 'flex justify-center'} max-w-10 w-full h-full`}
                 >
                   <img
-                    src={security}
+                    src={isActive('/apartment') ? security : security}
                     alt=""
                     className="max-w-5 max-h-5 object-contain"
                   />
                 </figure>
                 {menu ? (
-                  <span className="font-semibold text-primary-color">
+                  <span className={`font-semibold text-primary-color ${isActive('/security') && 'text-white'}`}>
                     Security
                   </span>
                 ) : (
@@ -171,20 +180,20 @@ const Sidebar = () => {
             <li className="w-full h-fit flex flex-col cursor-pointer">
               <div
                 onClick={handleDocs}
-                className={`flex items-center justify-between w-4/5  m-auto h-auto hover:bg-primary-color/5 rounded-md ${menu ? 'p-5' : 'p-3'}`}
+                className={`flex items-center justify-between gap-5 w-4/5  m-auto h-auto hover:bg-primary-color/5 rounded-md ${menu ? 'p-5' : 'p-3'} ${isActive('/document/lease-agreement') ? 'bg-primary-color' : isActive('/document/invoice') ? 'bg-primary-color' : ''}`}
               >
                 <div className="flex justify-between items-center gap-5">
                   <figure
-                    className={`${menu ? '' : 'flex justify-center'} max-w-10 w-full h-full`}
+                    className={`${menu ? '' : 'flex justify-center'} max-w-5 max-h-5 w-full h-full`}
                   >
                     <img
-                      src={document_svg}
+                      src={isActive('/document/lease-agreement') ? document_ : isActive('/document/invoice') ? document_ : documents}
                       alt=""
-                      className="max-w-10 max-h-10 object-contain"
+                      className="size-full  object-contain"
                     />
                   </figure>
                   {menu ? (
-                    <span className="font-semibold text-primary-color">
+                    <span className={`font-semibold text-primary-color ${isActive('/document/lease-agreement') ? 'text-white' :isActive('/document/invoice') ? 'text-white' : ''} `}>
                       Document
                     </span>
                   ) : (
@@ -206,7 +215,7 @@ const Sidebar = () => {
               <div className={`${docs ? '' : 'hidden'} w-full m-auto`}>
                 <ul className="flex flex-col">
                   <li
-                    className={`flex items-center center w-4/5  m-auto h-full hover:bg-primary-color/5 rounded-md `}
+                    className={`flex items-center ${!menu && 'justify-center' } w-4/5  m-auto h-full hover:bg-primary-color/5 rounded-md `}
                   >
                     <Link
                       className={`${menu ? 'p-3' : 'p-3'} w-full h-full`}
@@ -231,19 +240,19 @@ const Sidebar = () => {
             <li className="w-full h-fit">
               <Link
                 to={`/concern&issue`}
-                className={`flex items-center center w-4/5  mx-auto h-full hover:bg-primary-color/5 rounded-md ${menu ? 'p-5' : 'p-3'}`}
+                className={`flex items-center gap-5 ${!menu && 'justify-center' } w-4/5  mx-auto h-full hover:bg-primary-color/5 rounded-md ${menu ? 'p-5' : 'p-3'} ${isActive('/concern&issue') ? 'bg-primary-color' : ''}`}
               >
                 <figure
-                  className={`${menu ? '' : 'flex justify-center'} max-w-10 w-full h-full`}
+                  className={`${menu ? '' : 'flex justify-center'} max-w-5  max-h-5 w-full h-full`}
                 >
                   <img
-                    src={report}
+                    src={isActive('/concern&issue') ? report_ : report}
                     alt=""
-                    className="max-w-5 max-h-5 object-contain"
+                    className="size-full object-contain"
                   />
                 </figure>
                 {menu ? (
-                  <span className="font-semibold text-primary-color">
+                  <span className={`font-semibold text-primary-color ${isActive('/concern&issue') ? 'text-white' : ''}`}>
                     Reports and Concern
                   </span>
                 ) : (
@@ -254,19 +263,19 @@ const Sidebar = () => {
             <li className="w-full h-fit">
               <Link
                 to={`/terms&condition`}
-                className={`flex items-center center w-4/5  mx-auto h-full hover:bg-primary-color/5 rounded-md ${menu ? 'p-5' : 'p-3'}`}
+                className={`flex items-center ${!menu && 'justify-center' } w-4/5 gap-5  mx-auto h-full hover:bg-primary-color/5 rounded-md ${menu ? 'p-5' : 'p-3'} ${isActive('/terms&condition') && 'bg-primary-color'}`}
               >
                 <figure
-                  className={`${menu ? '' : 'flex justify-center'} max-w-10 w-full h-full`}
+                  className={`${menu ? '' : 'flex justify-center'} max-w-5 max-h-5 w-full h-full`}
                 >
                   <img
-                    src={term}
+                    src={isActive('/terms&condition') ? term_ : term}
                     alt=""
-                    className="max-w-5 max-h-5 object-contain"
+                    className="size-full object-contain"
                   />
                 </figure>
                 {menu ? (
-                  <span className="font-semibold text-primary-color">
+                  <span className={`font-semibold text-primary-color ${isActive('/terms&condition') && 'text-white'}`}>
                     Terms & Condition
                   </span>
                 ) : (
@@ -277,19 +286,19 @@ const Sidebar = () => {
             <li className="w-full h-fit">
               <Link
                 to={`/contact`}
-                className={`flex items-center center w-4/5  mx-auto h-full hover:bg-primary-color/5 rounded-md ${menu ? 'p-5' : 'p-3'}`}
+                className={`flex items-center ${!menu && 'justify-center' } w-4/5 gap-5  mx-auto h-full hover:bg-primary-color/5 rounded-md ${menu ? 'p-5' : 'p-3'} ${isActive('/contact') && 'bg-primary-color'}`}
               >
                 <figure
-                  className={`${menu ? '' : 'flex justify-center'} max-w-10 w-full h-full`}
+                  className={`${menu ? '' : 'flex justify-center'} max-w-5 max-h-5 w-full h-full`}
                 >
                   <img
-                    src={contact}
+                    src={isActive('/contact') ? contact_ : contact}
                     alt=""
-                    className="max-w-5 max-h-5 object-contain"
+                    className="size-full object-contain"
                   />
                 </figure>
                 {menu ? (
-                  <span className="font-semibold text-primary-color">
+                  <span className={`font-semibold text-primary-color ${isActive('/contact') && 'text-white'}`}>
                     Contact Us
                   </span>
                 ) : (
@@ -300,15 +309,15 @@ const Sidebar = () => {
             <li className="w-full h-fit md:[&:last-child]:absolute bottom-5">
               <button
                 onClick={handleLogout}
-                className={`flex items-center center w-4/5  mx-auto h-full hover:bg-primary-color/5 rounded-md ${menu ? 'p-5' : 'p-3'}`}
+                className={`flex items-center ${!menu && 'justify-center' } w-4/5 gap-5 mx-auto h-full hover:bg-primary-color/5 rounded-md ${menu ? 'p-5' : 'p-3'}`}
               >
                 <figure
-                  className={`${menu ? '' : 'flex justify-center'} max-w-10 w-full h-full`}
+                  className={`${menu ? '' : 'flex justify-center'}  max-w-5 max-h-5 w-full h-full`}
                 >
                   <img
                     src={logout}
                     alt=""
-                    className="max-w-5 rotate-180 max-h-5 object-contain"
+                    className="rotate-180 size-full object-contain"
                   />
                 </figure>
                 {menu ? (
