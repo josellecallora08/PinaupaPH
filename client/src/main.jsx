732107c6bcd042a181e8ba71f9222761ec19259a
soj <josellecallora08@gmail.com>
2024-05-02 10:01:28 +0800

    added packagejson

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
-import {store} from './app/store.js'
-import {Provider} from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import { store } from './app/store.js'
+import { Provider } from 'react-redux'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
