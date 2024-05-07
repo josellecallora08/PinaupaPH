cb07af6b58a11a810185ba774aa2fa95a9ede5cb
Rj <rolandangeles10@gmail.com>
2024-05-07 22:05:12 +0800

    Rj Added

--- server/routes/notification.js
+++ server/routes/notification.js
@@ -11,7 +11,7 @@ const router = Router()
 
 router.get('/list', fetchNotifications)
 router.get('/list/v1', requireAuth, fetchNotification)
-router.update('/update', requireAuth, readNotification)
+router.patch('/patch', requireAuth, readNotification)
 router.delete('/delete', deleteNotification)
 
 module.exports = router
