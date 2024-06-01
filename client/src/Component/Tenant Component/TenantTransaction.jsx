8b5b733f24a328999f949039175e284cb20545b4
Rj <rolandangeles10@gmail.com>
2024-06-01 22:59:48 +0800

    Rj Added

--- client/src/Component/Tenant Component/TenantTransaction.jsx
+++ client/src/Component/Tenant Component/TenantTransaction.jsx
@@ -8,7 +8,10 @@ const TenantTransaction = () => {
 
   return (
     <div>
-      <div className="h-full w-full">
+      <div className="h-full lg:w-11/12 w-full m-auto">
+      <div className="lg:mt-10 mt-5 uppercase font-bold  mx-4">
+          <h1 className="">Payment Transaction</h1>
+        </div>
         <div className="lg:block hidden">
           <TenantTableTransaction tenant={tenant} />
         </div>
